1ca5f162961f5fa3b0ec0dec206665db6d1fc371
Paal Arthur S. Thorseth <paalthorseth@gmail.com>
2021-01-18 18:17:43 +0100

    Remove build and devel folders

--- catkin_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/paal/Tetrapod-Robot/catkin_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
