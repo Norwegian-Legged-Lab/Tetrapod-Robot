17673ac9c140af8de7de5f771c8a74a34e2bda11
Adrian <adrianghnsh@gmail.com>
2021-03-15 13:35:57 +0100

    Add initial main function for Teensy

---
+++ Arduino/teensy_main/main/main.ino
@@ -0,0 +1,16 @@
+#include "motor_control.h"
+
+MotorControl motor1(1);
+
+void setup() {
+  Serial.begin(115200);
+  delay_microseconds(100000.0);
+}
+
+void loop() {
+  motor1.setSpeedReference(2*M_PI);
+  Serial.println("Speed set");
+
+  delay_microseconds(10*1000000.0);
+  while(true);
+}
