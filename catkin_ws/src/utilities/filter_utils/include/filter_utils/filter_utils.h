2575e106ab0bf83268d4fc34e4db07095bc004ae
Adrian Ghansah <adrianghnsh@gmail.com>
2021-04-25 16:23:46 +0200

    Add extra functionality to filter utils

--- catkin_ws/src/utilities/filter_utils/include/filter_utils/filter_utils.h
+++ catkin_ws/src/utilities/filter_utils/include/filter_utils/filter_utils.h
@@ -6,9 +6,13 @@
 class ThirdOrderFilter
 {
 public:
+    ThirdOrderFilter(){}
     ThirdOrderFilter(double _dt, double _x_current, double _x_goal, double _omega, double _zeta);
     void setReference(double _x_goal){x_goal = _x_goal;};
     void updateFilter();
+    void setParameters(double _frequency, double _damping);
+    double getSpeed() {return X(1);}
+    void setCurrentPos(double _x_current){X(0) = _x_current;}
     Eigen::Matrix<double, 3, 1> getState() {return X;}
 private:
     Eigen::Matrix<double, 3, 1> X;
