3203c98471e2c6cc3bd580ef1c4fa6d5e8e3f0c0
Adrian Ghansah <adrianghnsh@gmail.com>
2021-04-25 16:24:45 +0200

    Add initial single leg test controller. Not ready

---
+++ catkin_ws/src/control/single_leg_controller_test/include/single_leg_controller_test/single_leg_controller_test.h
@@ -0,0 +1,126 @@
+#ifndef single_leg_controller_h
+#define single_leg_controller_h
+
+// ROS
+#include "ros/ros.h"
+#include "std_msgs/Empty.h"
+#include "std_msgs/Bool.h"
+#include "sensor_msgs/JointState.h"
+// Eigen
+#include <Eigen/Core>
+
+// Standard Library
+#include <thread>
+
+// Kinematics
+#include <kinematics/kinematics.h>
+
+// Utilities
+#include <filter_utils/filter_utils.h>
+
+class SingleLegController
+{
+    public:
+        /// \brief Constructor
+        SingleLegController();
+
+        /// \brief Destructor
+        virtual ~SingleLegController();
+
+        /// \brief The joint states for the leg is updated
+        /// \param[in] _msg A float array containing the generalized coordinates 
+        void generalizedCoordinatesCallback(const sensor_msgs::JointStateConstPtr &_msg);
+
+        /// \brief The ready_to_move parameter is changed based on the incomming message
+        /// \param[in] _msg A bool message deciding if it is safe to move the leg or not
+        void readyToMoveCallback(const std_msgs::Bool &_msg);
+
+        /// \brief Updates the joint speed control commands based on foot vel references and jacobian
+        /*/// \return If we failed to update the speed commands report false. Otherwise return true*/
+        void updateSpeedControlCommands();
+
+        /// \brief Set the frequency and damping parameters for the three references
+        /// \param[in] _omega_x Frequency of x reference
+        /// \param[in] _omega_y Frequency of y reference
+        /// \param[in] _omega_z Frequency of z reference
+        /// \param[in] _damping_x Damping of x reference
+        /// \param[in] _damping_y Damping of y reference
+        /// \param[in] _damping_z Damping of z reference
+        void setReferenceParameters
+        (
+            double _omega_x, double _omega_y, double _omega_z,
+            double _damping_x, double _damping_y, double _damping_z
+        );
+
+        /// \brief Set the current value of the references
+        /// \param[in] _pos_x Set the current x position
+        /// \param[in] _pos_y Set the current y position
+        /// \param[in] _pos_z Set the current z position
+        void setCurrentReferencePosition(double _pos_x, double _pos_y, double _pos_z);
+
+        /// \brief Set the foot target velocity
+        /// \param[in] _desired_foot_vel_x The desired foot speed in the x direction
+        /// \param[in] _desired_foot_vel_y The desired foot speed in the y direction
+        /// \param[in] _desired_foot_vel_z The desired foot speed in the z direction
+        void setFootGoalPos(double _desired_foot_pos_x, double _desired_foot_pos_y, double _desired_foot_pos_z);
+
+        /// \brief This is a ROS helper function that process messages.
+        //void processQueueThread();
+
+        /// \brief This is a ROS helper function that publish messages.
+        //void publishQueueThread();
+
+        /// \brief This function initilizes ROS
+        void initROS();
+
+        /// \brief This function initializes the ROS publish and process queue threads
+        //void initROSQueueThreads();
+
+    private:
+        /// \brief The joint angles of the leg
+        Eigen::Matrix<double, 3, 1> joint_angles = Eigen::Matrix<double, 3, 1>::Zero();
+
+        /// \brief Desired joint velocities
+        Eigen::Matrix<double, 3, 1> joint_velocities = Eigen::Matrix<double, 3, 1>::Zero();
+
+        /// \brief ROS node handle
+        std::unique_ptr<ros::NodeHandle> nodeHandle;
+
+        /// \brief Subscribes to generalized coordinates messages
+        ros::Subscriber generalizedCoordinatesSubscriber;
+
+        /// \brief Subscribes to messages deciding whether or not we should move the leg
+        ros::Subscriber readyToMoveSubscriber;
+
+        /// \brief Publishes velocity commands to the teensy to control the motors
+        ros::Publisher jointCommandPublisher;
+
+        /// \brief ROS callback queue that helps processing messages
+        //ros::CallbackQueue rosProcessQueue;
+
+        /// \brief ROS callback queue that helps publishing messages
+        //ros::CallbackQueue rosPublishQueue;
+
+        /// \brief Thread running the ROS process queue
+        //std::thread rosProcessQueueThread;
+
+        /// \brief Thread running the ROS publish queue
+        //std::thread rosPublishQueueThread;
+
+        /// \brief Reference generator for foot speed in x direction
+        ThirdOrderFilter filter_ref_foot_speed_x;
+
+        /// \brief Reference generator for foot speed in y direction
+        ThirdOrderFilter filter_ref_foot_speed_y;
+
+        /// \brief Reference generator for foot speed in z direction
+        ThirdOrderFilter filter_ref_foot_speed_z;
+
+        /// \brief Variable deciding whether or not we want to move the leg
+        bool ready_to_move = false;
+
+        /// \brief Variable indicating wheher or not we have reached the goal
+        bool goal_reached = false;
+};
+
+#endif
---
+++ catkin_ws/src/control/single_leg_controller_test/src/single_leg_controller_test.cpp
@@ -0,0 +1,104 @@
+#include "single_leg_controller_test/single_leg_controller_test.h"
+
+void SingleLegController::generalizedCoordinatesCallback(const sensor_msgs::JointStateConstPtr &_msg)
+{
+    for(int i = 0; i < 3; i++)
+    {
+        joint_angles(i) = _msg->position[i];
+    }
+}
+
+void SingleLegController::readyToMoveCallback(const std_msgs::Bool &_msg)
+{
+    ready_to_move = _msg.data;
+}
+
+int main(int argc, char **argv)
+{
+    SingleLegController single_leg_controller;
+
+    ros::spin();
+
+    return 0;
+}
+
+void SingleLegController::initROS()
+{
+    // Check if ROS has been initialized. If false, initialize ROS.
+    if(!ros::isInitialized())
+    {
+        int argc = 0;
+        char **argv = NULL;
+        ros::init
+        (
+            argc,
+            argv,
+            "single_leg_controller_test_node"//,
+            //ros::init_options::NoSigintHandler
+        );
+    }
+
+    // WHY DO THIS?
+    nodeHandle.reset(new ros::NodeHandle("single_leg_controller_test_node"));
+
+    generalizedCoordinatesSubscriber = nodeHandle->subscribe
+    (
+        "/generalized_coordinates", 
+        10, 
+        &SingleLegController::generalizedCoordinatesCallback,
+        this
+    );
+
+    readyToMoveSubscriber = nodeHandle->subscribe
+    (
+        "/ready_to_move",
+        1, 
+        &SingleLegController::readyToMoveCallback,
+        this
+    );
+
+    jointCommandPublisher = nodeHandle->advertise<sensor_msgs::JointState>("/joint_command", 10);
+}
+
+void SingleLegController::setFootGoalPos(double _desired_foot_pos_x, double _desired_foot_pos_y, double _desired_foot_pos_z)
+{
+    filter_ref_foot_speed_x.setReference(_desired_foot_pos_x);
+    filter_ref_foot_speed_y.setReference(_desired_foot_pos_y);
+    filter_ref_foot_speed_z.setReference(_desired_foot_pos_z);
+}
+
+void SingleLegController::setReferenceParameters
+(
+    double _omega_x, double _omega_y, double _omega_z,
+    double _damping_x, double _damping_y, double _damping_z
+)
+{
+    filter_ref_foot_speed_x.setParameters(_omega_x, _damping_x);
+    filter_ref_foot_speed_y.setParameters(_omega_y, _damping_y);
+    filter_ref_foot_speed_z.setParameters(_omega_z, _damping_z);
+}
+
+void SingleLegController::setCurrentReferencePosition(double _pos_x, double _pos_y, double _pos_z)
+{
+    filter_ref_foot_speed_x.setCurrentPos(_pos_x);
+    filter_ref_foot_speed_y.setCurrentPos(_pos_y);
+    filter_ref_foot_speed_z.setCurrentPos(_pos_z);
+}
+
+void SingleLegController::updateSpeedControlCommands()
+{
+    filter_ref_foot_speed_x.updateFilter();
+    filter_ref_foot_speed_y.updateFilter();
+    filter_ref_foot_speed_z.updateFilter();
+
+    Eigen::Matrix<double, 3, 1> vel_foot;
+    vel_foot(0) = filter_ref_foot_speed_x.getSpeed();
+    vel_foot(1) = filter_ref_foot_speed_y.getSpeed();
+    vel_foot(2) = filter_ref_foot_speed_z.getSpeed();
+
+    Eigen::Matrix<double, 3, 3> foot_jacobian = Eigen::Matrix<double, 3, 3>::Zero();
+    //foot_jacobian = kinematics::getFootJacobian(joint_angles); 
+    for(int i = 0; i < 3; i++){foot_jacobian(i, i) = 1.0;} // TODO Replace with true Jacobian
+
+    joint_velocities = foot_jacobian.inverse()*vel_foot;
+}
