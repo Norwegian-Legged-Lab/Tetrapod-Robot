4027857634f84bd64515e89da15406915ab8675f
Adrian <adrianghnsh@gmail.com>
2021-05-05 07:24:50 +0200

    Add static package. Incomplete

---
+++ catkin_ws/src/control/static_gait_controller/include/static_gait_controller/static_gait_controller.h
@@ -0,0 +1,218 @@
+#ifndef static_gait_h
+#define static_gait_h
+
+// ROS
+#include "ros/ros.h"
+#include "std_msgs/Empty.h"
+#include "std_msgs/Bool.h"
+#include "std_msgs/Float64.h"
+#include "std_msgs/Float64MultiArray.h"
+#include "sensor_msgs/JointState.h"
+#include "eigen_conversions/eigen_msg.h"
+
+// Eigen
+#include <Eigen/Core>
+
+// Standard Library
+#include <thread>
+
+// Kinematics
+#include <kinematics/kinematics.h>
+
+// Utilities
+#include <filter_utils/filter_utils.h>
+
+/// \brief Variable indicate no control effort
+const double CONTROL_IDLE  = 1000.0;
+
+/// \brief Convergence crieria for position control test
+const double POSITION_CONVERGENCE_CRITERIA = 0.010; // Cirka 1 degree error for all joints
+
+class StaticGaitController
+{
+    public:
+        /// \brief Constructor
+        StaticGaitController(double _dt);
+
+        /// \brief Destructor
+        virtual ~StaticGaitController(){};
+
+        /// \brief The joint states for the leg is updated
+        /// \param[in] _msg A float array containing the generalized coordinates 
+        void generalizedCoordinatesCallback(const sensor_msgs::JointStateConstPtr &_msg);
+
+        /// \brief The ready_to_move parameter is changed based on the incomming message
+        /// \param[in] _msg A bool message deciding if it is safe to move the leg or not
+        void readyToMoveCallback(const std_msgs::Bool &_msg);
+
+        /// \brief Updates the joint position control commands based on the desired foot position
+        void sendJointPositionCommand();
+
+        /// \brief Updates the joint speed control commands based on foot vel references and jacobian
+        /*/// \return If we failed to update the speed commands report false. Otherwise return true*/
+        void updateSpeedControlCommands();
+
+        /// \brief Populates the joint command message with joint velocity commands and sends it to the Teensy
+        void sendSpeedJointCommand();
+
+        /// \brief Set the frequency and damping parameters for the three references
+        /// \param[in] _omega_x Frequency of x reference
+        /// \param[in] _omega_y Frequency of y reference
+        /// \param[in] _omega_z Frequency of z reference
+        /// \param[in] _damping_x Damping of x reference
+        /// \param[in] _damping_y Damping of y reference
+        /// \param[in] _damping_z Damping of z reference
+        void setReferenceParameters
+        (
+            double _omega_x, double _omega_y, double _omega_z,
+            double _damping_x, double _damping_y, double _damping_z
+        );
+
+        /// \brief Set the current value of the references
+        /// \param[in] _pos_x Set the current x position
+        /// \param[in] _pos_y Set the current y position
+        /// \param[in] _pos_z Set the current z position
+        void setCurrentReferencePosition(double _pos_x, double _pos_y, double _pos_z);
+
+        /// \brief Set the foot target velocity
+        /// \param[in] _desired_foot_vel_x The desired foot speed in the x direction
+        /// \param[in] _desired_foot_vel_y The desired foot speed in the y direction
+        /// \param[in] _desired_foot_vel_z The desired foot speed in the z direction
+        void setFootGoalPos(double _desired_foot_pos_x, double _desired_foot_pos_y, double _desired_foot_pos_z);
+
+        /// \brief The function tries to move the foot to the position given by the input parameters.
+        /// This is done by calculating the inverse kinematics and giving the motors position references
+        /// \param[in] _foot_pos_x The target foot x position
+        /// \param[in] _foot_pos_y The target foot y position
+        /// \param[in] _foot_pos_z The target foot z position
+        bool moveFootToPosition(double _foot_pos_x, double _foot_pos_y, double _foot_pos_z);
+
+        /// \brief Function used to check if any new ROS messages has been received
+        void checkForNewMessages();
+
+        /// \brief This is a ROS helper function that process messages.
+        //void processQueueThread();
+
+        /// \brief This is a ROS helper function that publish messages.
+        //void publishQueueThread();
+
+        /// \brief This function initilizes ROS
+        void initROS();
+
+
+
+        /// \brief This functions moves each of the robot's legs so that it is ready to perform locomotion
+        void setInitialConfiguration();
+
+        /// \brief This variable keeps track of which leg to move
+        private: int gait_phase;
+
+        /// \brief This functions updates the joint commands based on the previous state and latest command
+        public: bool updateJointCommands();
+
+        /// \brief This function publishes the latest joint commands to the motors
+        public: void publishJointCommands();
+
+        /// \brief Distance from CoM to foot in y direction in the body frame
+        private: double step_width;
+
+        /// \brief Length of each step in the body x direction
+        private: double step_length;
+
+        /// \brief Minimum distance between the body and foot in the x direction
+        private: double x_offset_min;
+
+        /// \brief Maximum distance between the body and foot in the x direction
+        private: double x_offset_max;
+
+        /// \brief Desired feet positions in the body frame
+        private: Eigen::Matrix<double, 4, 1> feet_positions_in_body;
+
+        /// \brief Calculates the desired footstep positions in the body frame
+        public: bool updateFeetPositionsInBody();
+
+    private:
+        /// \brief The joint angles of the leg
+        Eigen::Matrix<double, 12, 1> joint_angles = Eigen::Matrix<double, 12, 1>::Zero();
+
+        /// \brief Desired joint velocity
+        Eigen::Matrix<double, 12, 1> joint_velocities = Eigen::Matrix<double, 12, 1>::Zero();
+
+        /// \brief ROS node handle
+        std::unique_ptr<ros::NodeHandle> nodeHandle;
+
+        /// \brief Subscribes to generalized coordinates messages
+        ros::Subscriber generalizedCoordinatesSubscriber;
+
+        /// \brief Subscribes to messages deciding whether or not we should move the leg
+        ros::Subscriber readyToMoveSubscriber;
+
+        /// \brief Publishes velocity commands to the teensy to control the motors
+        ros::Publisher jointCommandPublisher;
+
+        /// \brief The joint state message that is sent to the Teensy
+        sensor_msgs::JointState motor_command_msg;
+
+        /// \brief ROS callback queue that helps processing messages
+        //ros::CallbackQueue rosProcessQueue;
+
+        /// \brief ROS callback queue that helps publishing messages
+        //ros::CallbackQueue rosPublishQueue;
+
+        /// \brief Thread running the ROS process queue
+        //std::thread rosProcessQueueThread;
+
+        /// \brief Thread running the ROS publish queue
+        //std::thread rosPublishQueueThread;
+
+        /// \brief Reference generator for foot speed in x direction
+        ThirdOrderFilter filter_ref_foot_speed_x;
+
+        /// \brief Reference generator for foot speed in y direction
+        ThirdOrderFilter filter_ref_foot_speed_y;
+
+        /// \brief Reference generator for foot speed in z direction
+        ThirdOrderFilter filter_ref_foot_speed_z;
+
+        /// \brief Variable deciding whether or not we want to move the leg
+        bool ready_to_move = false;
+
+        /// \brief Variable indicating wheher or not we have reached the goal
+        bool is_target_position_reached = false;
+
+        /// \brief Angles used for the joint position setpoint controller
+        Eigen::Matrix<double, 3, 1> position_controller_joint_target = Eigen::Matrix<double, 3, 1>::Zero();
+
+        /// \brief Setpoint joint velocity for velocity control
+        Eigen::Matrix<double, 3, 1> velocity_controller_joint_target = Eigen::Matrix<double, 3, 1>::Zero();
+
+        /// \brief Kinematics object
+        Kinematics kinematics;
+
+
+        //*** FOR SIMULATOR ***//
+
+        /// \brief Simulator joint angles
+        //Eigen::Matrix<double, 12, 1> simulator_joint_angles;
+
+        ros::Subscriber simulator_generalized_coordinates_subscriber;
+
+        ros::Subscriber simulator_generalized_velocity_subscriber;
+
+        ros::Publisher simulator_joint_state_publisher;
+
+        void simulatorGeneralizedCoordinateCallback(const std_msgs::Float64MultiArrayConstPtr &_msg);
+
+        void simulatorGeneralizedVelocityCallback(const std_msgs::Float64MultiArrayConstPtr &_msg);
+
+        void simulatorSendJointPositionCommand();
+
+    public: void simulatorSendJointVelocityCommand();
+
+    //*** For logging ***//
+    private:    ros::Publisher log_state_publisher;
+
+    private:    ros::Publisher log_command_publisher;
+};
+
+#endif
---
+++ catkin_ws/src/control/static_gait_controller/src/static_gait_node.cpp
@@ -0,0 +1,134 @@
+#include "ros/ros.h"
+#include <ros/console.h>
+
+#include "static_gait_controller/static_gait_controller.h"
+
+#define FREQUENCY_X 5.0
+#define FREQUENCY_Y 2.0
+#define FREQUENCY_Z 8.0
+#define DAMPING_X 1.0
+#define DAMPING_Y 1.0
+#define DAMPING_Z 1.0
+
+#define START_POS_X 0.433
+#define START_POS_Y 0.25
+#define START_POS_Z -0.25 
+
+#define MIDDLE_POS_X 0.0
+#define MIDDLE_POS_Y 0.25
+#define MIDDLE_POS_Z -0.10
+
+#define END_POS_X -0.433
+#define END_POS_Y 0.25
+#define END_POS_Z -0.25
+
+#define TIMESTEP 0.01
+#define TIME_END 2.0
+#define TIME_MIDDLE 1.0
+/*
+int main(int argc, char **argv)
+{
+    StaticGaitController single_leg_controller(TIMESTEP);
+
+    single_leg_controller.setReferenceParameters
+    (
+        FREQUENCY_X, FREQUENCY_Y, FREQUENCY_Z,
+        DAMPING_X, DAMPING_Y, DAMPING_Z
+    );
+
+    single_leg_controller.setCurrentReferencePosition(START_POS_X, START_POS_Y, START_POS_Z);
+
+    single_leg_controller.setFootGoalPos(MIDDLE_POS_X, MIDDLE_POS_Y, MIDDLE_POS_Z);
+    
+    single_leg_controller.initROS();
+
+    int timestep_middle = TIME_MIDDLE/TIMESTEP;
+
+    int timestep_end = TIME_END/TIMESTEP;
+
+    int current_timestep = 0;
+
+    ROS_INFO("Initialization complete");
+
+    ros::Rate check_for_ready_message_rate(1);
+
+    ros::Rate send_control_command_rate(1.0/TIMESTEP);
+
+    // Wait for a confirmation message before moving on
+    while(!single_leg_controller.checkIfReadyToMove())
+    {
+        single_leg_controller.checkForNewMessages();
+
+        ros::spinOnce();
+
+        check_for_ready_message_rate.sleep();
+
+        ROS_INFO("WAITING FOR POSITION CONTROL START MESSAGE");
+    }
+
+    // Use the position controller to move the leg to the desired start position
+    while(!single_leg_controller.moveFootToPosition(START_POS_X, START_POS_Y, START_POS_Z));
+
+    ROS_INFO("DESIRED POSITION REACHED");
+    
+    // Reset the is_target_position_reached flag 
+    single_leg_controller.setTargetPositionToNotReached();
+    
+    // Reset the ready_to_move flag
+    single_leg_controller.setNotReadyToMove();
+
+    // Wait for a confirmation message before starting speed control
+    while(!single_leg_controller.checkIfReadyToMove())
+    {
+        single_leg_controller.checkForNewMessages();
+
+        check_for_ready_message_rate.sleep();
+
+        ROS_INFO("WAITING FOR START MESSAGE FOR VELOCITY CONTROL");
+    }
+
+    // Use velocity control to move the foot to the mid position
+    while(current_timestep < timestep_middle)
+    {
+        single_leg_controller.updateSpeedControlCommands();
+
+        //single_leg_controller.sendSpeedJointCommand();
+
+        single_leg_controller.simulatorSendJointVelocityCommand();
+
+        current_timestep++;
+
+        single_leg_controller.logStatesAndCommands();
+
+        send_control_command_rate.sleep();
+    }
+
+    // Use velocity control to move the foot to the end position
+    single_leg_controller.setFootGoalPos(END_POS_X, END_POS_Y, END_POS_Z);
+
+    while(current_timestep < timestep_end)
+    {
+        single_leg_controller.updateSpeedControlCommands();
+
+        //single_leg_controller.sendSpeedJointCommand();
+
+        single_leg_controller.simulatorSendJointVelocityCommand();
+
+        ros::spinOnce();
+
+        current_timestep++;
+
+        single_leg_controller.logStatesAndCommands();
+
+        send_control_command_rate.sleep();
+    }
+
+    single_leg_controller.setJointVelocityToZero();
+    
+    ROS_INFO("COMPLETE");
+
+    ros::spinOnce();
+
+    return 0;
+}
+*/
