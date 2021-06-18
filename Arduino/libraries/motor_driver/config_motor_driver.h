55a4d89f3f1acf458fcdbc83d38d401203b89110
Adrian Ghansah <adrianghnsh@gmail.com>
2021-06-18 19:46:24 +0200

    Small changes in configurations

--- Arduino/libraries/motor_driver/config_motor_driver.h
+++ Arduino/libraries/motor_driver/config_motor_driver.h
@@ -1,10 +1,12 @@
 #ifndef motor_driver_config_h
 #define motor_driver_config_h
 
+#include "Arduino.h"
+
 #define ROS_PRINT false
 #define SERIAL_PRINT true
 
-#define FRONT_LEGS false
+#define FRONT_LEGS true
 #define REAR_LEGS false
 
 // Calculate offsets based on legs
@@ -12,13 +14,13 @@
 // An offset of -30 degrees is added to each actuator to compensate for the encoder zero position 
 
 #if (FRONT_LEGS)
-    #define position_offset_motor_1 0.0
+    #define position_offset_motor_1 M_PI/2.0 // 45 degrees
     #define position_offset_motor_2 0.0
     #define position_offset_motor_3 0.0
-    #define position_offset_motor_4 0.0
+    #define position_offset_motor_4 -M_PI/4.0 // -45 degrees
     #define position_offset_motor_5 0.0
     #define position_offset_motor_6 0.0
-#elif (REAR_LEGS)
+#elif(REAR_LEGS)
     #define position_offset_motor_1 0.0
     #define position_offset_motor_2 0.0
     #define position_offset_motor_3 0.0
