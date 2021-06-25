41255a18eed2af5c63493928460fa27a622ff514
Adrian Ghansah <adrianghnsh@gmail.com>
2021-06-25 16:28:56 +0200

    Added error info to single leg plot

--- logs/single_leg_plotting.m
+++ logs/single_leg_plotting.m
@@ -1,8 +1,15 @@
 close all
 clear
-clc
+%clc
 
-timestamp = "2021-05-24-13-35-45";
+timestamp = "2021-06-23-11-05-59"; % Rate: 50, k_pos: 3, k_vel: 10, k_tor: 20 
+timestamp = "2021-06-23-11-20-28"; % Rate: 200, k_pos: 3, k_vel: 10, k_tor: 20 
+%timestamp = "2021-06-23-11-25-17"; % Rate: 200, k_pos: 10, k_vel: 25, k_tor: 50 
+
+%timestamp = "2021-06-25-15-28-17"; % Subtract Coriolis - Torque Control
+%timestamp = "2021-06-25-16-12-37"; % Add Coriolis - Torque Control
+%timestamp = "2021-06-25-16-18-42"; % Add Coriolis - Position control
+
 
 number_of_motors = 3;
 
@@ -19,7 +26,7 @@ joint_torques = get_joint_effort_states(state_bag, number_of_motors);
 reference_time = get_time_states(reference_bag);
 joint_pos_reference = get_joint_position_states(reference_bag, number_of_motors);
 joint_vel_reference = get_joint_velocity_states(reference_bag, number_of_motors);
-joint_acc_reference = get_joint_effort_states(reference_bag, number_of_motors);
+joint_torque_reference = get_joint_effort_states(reference_bag, number_of_motors);
 
 %% Remove time offset from the measurements so that time zero is when the first message of one time is received
 time_offset = 0;
@@ -60,7 +67,6 @@ joint_vel_state = joint_vel_state*180.0/pi;
 
 joint_pos_reference = joint_pos_reference*180.0/pi;
 joint_vel_reference = joint_vel_reference*180.0/pi;
-joint_acc_reference = joint_acc_reference*180.0/pi;
 
 %% Plot results
 
@@ -132,14 +138,31 @@ hold off
 
 % Joint Torques
 figure(3)
-hold on
+
+subplot(3, 1, 1)
+hold on 
 grid on
+plot(reference_time, joint_torque_reference(:, 1));
 plot(state_time, joint_torques(:, 1));
-plot(state_time, joint_torques(:, 2));
-plot(state_time, joint_torques(:, 3));
-legend("\tau_{hy}", "\tau_{hp}", "\tau_{kp}");
-xlabel("time [s]");
+legend("\tau_{hy, ref}", "\tau_{hy}");
+hold off
+
+subplot(3, 1, 2)
+hold on 
+grid on
+plot(reference_time, joint_torque_reference(:, 2));
+plot(state_time, joint_torques(:, 2)');
+legend("\tau_{hp, ref}", "\tau_{hp}");
 ylabel("torque [Nm]");
+hold off
+
+subplot(3, 1, 3)
+hold on 
+grid on
+plot(reference_time, joint_torque_reference(:, 3));
+plot(state_time, joint_torques(:, 3)');
+legend("\tau_{kp, ref}", "\tau_{kp}");
+xlabel("time [s]");
 hold off
 
 % Foot position 
@@ -215,7 +238,20 @@ zlabel("z [m]");
 hold off
 
 
+% Key data
+joint_pos_errors = sum(abs(joint_pos_reference - joint_pos_state))/i;
 
+joint_vel_errors = sum(abs(joint_vel_reference - joint_vel_state))/i;
+
+foot_pos_errors = sum(abs(foot_pos_ref - foot_pos_state))/i;
+
+foot_vel_errors = sum(abs(foot_vel_ref - foot_vel_state))/i;
+
+fprintf("Errors:\n");
+fprintf("Joint pos - hy: %f\thp: %f\tkp: %f\n", joint_pos_errors(1), joint_pos_errors(2), joint_pos_errors(3));
+fprintf("Joint vel - hy: %f\thp: %f\tkp: %f\n", joint_vel_errors(1), joint_vel_errors(2), joint_vel_errors(3));
+fprintf("Foot pos - x: %f\ty: %f\tz: %f\n", foot_pos_errors(1), foot_pos_errors(2), foot_pos_errors(3));
+fprintf("Foot vel - x: %f\ty: %f\tz: %f\n", foot_vel_errors(1), foot_vel_errors(2), foot_vel_errors(3));
 
 
 
