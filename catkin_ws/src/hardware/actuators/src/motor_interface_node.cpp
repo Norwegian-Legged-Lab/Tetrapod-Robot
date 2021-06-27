2c75acdf2c06515e98b5d29a085e883451406fe8
Adrian Ghansah <adrianghnsh@gmail.com>
2021-06-27 12:58:01 +0200

    Made temp changes to motor interface node

--- catkin_ws/src/hardware/actuators/src/motor_interface_node.cpp
+++ catkin_ws/src/hardware/actuators/src/motor_interface_node.cpp
@@ -6,7 +6,25 @@ int main(int argc, char **argv)
 {
     MotorInterface motor_interface(3);
 
-    ros::spin();
+    //ros::spin();
+
+    // Temporary solution
+    // START
+    ros::Rate loop_rate(10000);
+
+    while(ros::ok())
+    {
+        // Check for Serial replies
+        motor_interface.ProcessSerialMessages();
+
+        // Check for incomming command messages
+        ros::spinOnce();
+
+        // Sleep for a short duration
+        loop_rate.sleep();
+    }
+    // END
+    
 
     return 0;
 }
