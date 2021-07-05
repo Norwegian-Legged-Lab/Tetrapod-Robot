d079e183d511b353aa3e5f2b4866043dea431646
Mads Erlend Bøe Lysø <melyso@stud.ntnu.no>
2021-07-05 17:10:56 +0200

    Added publishing from footstep planner to Gazebo

--- catkin_ws/src/control/footstep_planner/src/footstep_planner_node.cpp
+++ catkin_ws/src/control/footstep_planner/src/footstep_planner_node.cpp
@@ -1,10 +1,11 @@
 #include "footstep_planner/footstep_planner.h"
 
-//#include "footstep_planner/waypoint_planner.h"
+#include "visualization_utils/xml_utils.h"
+#include "visualization_utils/eigen_geometry_msgs.h"
 
 #include "ros/ros.h"
-
 #include "ros/console.h"
+#include "geometry_msgs/PoseArray.h"
 
 bool footstep_planner_service(sensor_msgs::SetCameraInfoRequest &req, sensor_msgs::SetCameraInfoResponse &res){ return true;}
 
@@ -14,27 +15,29 @@ int main(int argc, char **argv)
 
     ros::NodeHandle n;
 
-    ros::ServiceServer service = n.advertiseService("footstep_planner", footstep_planner_service);
+    ros::Publisher footstep_pub = n.advertise<geometry_msgs::PoseArray>("/footstep_planner/footstep_plan", 1000);
+
+    ros::Duration(1).sleep();
 
     Eigen::Array<bool, 4, 1> bool_bridge;
 
     bool_bridge << true, true, true, true;
 
-    //Terrain terrain(bool_bridge);
-    Terrain terrain;
+    Terrain terrain(bool_bridge);
+    //Terrain terrain;
     terrain.print();
 
-    int n_steps = 4*24;
+    int n_steps = 4*20;
 
     int n_legs = 4;
 
     Leg step_sequence[] = {front_left, rear_right, front_right, rear_left};
 
-    double step_span = 0.4;
+    double step_span = 0.5;
 
-    double length_legs = 0.4;
+    double length_legs = 0.6;
 
-    double bbox_len = 0.3;
+    double bbox_len = 0.5;
 
     ROS_INFO("About to begin planning");
     
@@ -42,16 +45,22 @@ int main(int argc, char **argv)
     double wp_dist = 2;
     int n_points = 4;
     //DecVars_res res = waypoint_planner(terrain, n_points, length_legs, bbox_len, wp_dist);
+    if (res.success)
+    {
+        ROS_INFO("Writing to file...");
 
-    ROS_INFO("Got to the end");
+        writeDecVarsToFile(res, "/home/melyso/Documents/csv_files/footstep_planner");
+        
 
-    ROS_INFO("Writing to file...");
+        ROS_INFO("Successfully wrote to file!");
 
-    writeDecVarsToFile(res, "/home/melyso/Documents/csv_files/footstep_planner");
-    
-    ROS_INFO("Successfully wrote to file!");
+        geometry_msgs::PoseArray msg = pose_array_from_eigen_matr(res.position_ts);
+        ROS_INFO("Successfully got poseArray");
+        footstep_pub.publish(msg);    
+        ROS_INFO("Successfully published");
+    }
 
     ros::spinOnce();
-
+    
     return 0;
 }
