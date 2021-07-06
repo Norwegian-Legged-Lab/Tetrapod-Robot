1226cee5eac0b8322d679cddf2e64ba8c098c648
Paal Arthur S. Thorseth <paalthorseth@gmail.com>
2021-07-06 14:21:27 +0200

    Cleanup

---
+++ plot/single_leg_plot.m
@@ -0,0 +1,12 @@
+%% Init
+
+close all
+clear
+clc
+
+run("setup.m");
+
+bag_path = "~/Tetrapod-Robot/bagfiles/single_leg_tests/";
+figure_path = "~/Tetrapod-Robot/figures/single_leg_tests";
+
+number_of_motors = 1;
