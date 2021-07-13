9ab58a43aa67e65f0e690947c7f7e838a44f0450
Paal Arthur S. Thorseth <paalthorseth@gmail.com>
2021-07-13 11:22:32 +0200

    Update docs.

--- Arduino/libraries/utilities/utilities.h
+++ Arduino/libraries/utilities/utilities.h
@@ -8,6 +8,9 @@
 /// \param[in] _microseconds_delay Number of microseconds the program should wait
 void delay_microseconds(double _microseconds_delay);
 
+/// \brief The function takes in an array and prints the
+/// corresponding message to serial print.
+/// \param[in] _can_message CAN message.
 void printCANMessageData(unsigned char* _can_message);
 
 /// \brief The function takes in an array are converts the first 8 elements to zero
