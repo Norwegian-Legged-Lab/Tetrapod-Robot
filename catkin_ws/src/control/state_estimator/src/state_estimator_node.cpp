4d27438958eeebe91447f4fc18667591c68d1a5e
Adrian Ghansah <adrianghnsh@gmail.com>
2021-07-18 17:54:39 +0200

    Added simple state estimator for body vels

---
+++ catkin_ws/src/control/state_estimator/src/state_estimator_node.cpp
@@ -0,0 +1,27 @@
+#include "ros/ros.h"
+#include <ros/console.h>
+
+#include "state_estimator/state_estimator.h"
+
+
+int main(int argc, char **argv)
+{
+    int controller_frequency = 200;
+
+    double gait_period = 0.4;
+
+    StateEstimator state_estimator(controller_frequency, gait_period);
+
+    ros::Rate control_rate(controller_frequency);
+
+    while(ros::ok())
+    {   
+        ros::spinOnce();
+        state_estimator.UpdateStates();
+        state_estimator.PublishStates();
+        control_rate.sleep();
+    }
+    
+
+    return 0;
+}
