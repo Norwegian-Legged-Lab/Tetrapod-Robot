e69497da1ef15fa52fc123c62e1c5b8d1884d5cb
Paal Arthur S. Thorseth <paalthorseth@gmail.com>
2021-07-24 11:47:42 +0200

    Update base state plot.

--- plot/base_state_plot.m
+++ plot/base_state_plot.m
@@ -12,7 +12,7 @@ path = "~/Tetrapod-Robot/bagfiles/ho_tests/";
 % timestamp = "2021-07-19-19-11-37"; % Height and orientation test used for video
 % timestamp = "2021-07-20-07-45-31"; % Height and orientation test (4xt)
 % timestamp = "2021-07-20-08-07-53"; % Height, yaw, roll test (3xt)
-% timestamp = "2021-07-20-09-41-08"; % Height, yaw, roll test (3xt) used for video
+timestamp = "2021-07-20-09-41-08"; % Height, yaw, roll test (3xt) used for video
 % timestamp = "2021-07-21-15-31-44"; % Apply force test z35N
 % timestamp = "2021-07-21-14-13-01"; % Apply force test z200N
 
@@ -56,9 +56,10 @@ reference_time = reference_time' - time_offset;
 % Limits
 xlimit = [5, 25];
 base_pos_ylimits = {[-0.3,0.3], [-0.3,0.3], [0.05,0.35]};
-base_ori_ylimits = {[-40,40], [-10,10], [-20,20]};
 base_vel_ylimits = {[-0.4,0.4], [-0.4,0.4], [-0.4,0.4]};
-
+base_ori_ylimits = {[-40,40], [-10,10], [-20,20]};
+base_ang_vel_ylimits = {[-100,100], [-100,100], [-100,100]};
+base_2D_twist_ylimits = {[-1,1], [-1,1], [-100,100]};
 
 
 base_pos_legends = ["$x_{fb}$", "$y_{fb}$", "$z_{fb}$"];
@@ -66,15 +67,25 @@ base_pos_ref_legends = ["$x_{fb, ref}$", "$y_{fb, ref}$", "$z_{fb, ref}$"];
 base_pos_titles = ["\textbf{Floating base $x$ position response}", "\textbf{Floating base $y$ position response}", "\textbf{Floating base $z$ position response}"];
 base_pos_ylabels = ["$x$-coordinate [m]", "$y$-coordinate [m]", "$z$-coordinate [m]"];
 
+base_vel_legends = ["$\dot{x}_{fb}$", "$\dot{y}_{fb}$", "$\dot{z}_{fb}$"];
+base_vel_ref_legends = ["$\dot{x}_{fb, ref}$", "$\dot{y}_{fb, ref}$", "$\dot{z}_{fb, ref}$"];
+base_vel_titles = ["\textbf{Floating base $x$ velocity response}", "\textbf{Floating base $y$ velocity response}", "\textbf{Floating base $z$ velocity response}"];
+base_vel_ylabels = ["$x$-velocity [m/s]", "$y$-velocity [m/s]", "$z$-velocity [m/s]"];
+
 base_ori_legends = ["$\phi$", "$\theta$", "$\psi$"];
 base_ori_ref_legends = ["$\phi_{ref}$", "$\theta_{ref}$", "$\psi_{ref}$"];
 base_ori_titles = ["\textbf{Floating base roll response}", "\textbf{Floating base pitch response}", "\textbf{Floating base yaw response}"];
 base_ori_ylabels = ["Angle [deg]", "Angle [deg]", "Angle [deg]"];
 
-base_vel_legends = ["$\dot{x}_{fb}$", "$\dot{y}_{fb}$", "$\dot{z}_{fb}$"];
-base_vel_ref_legends = ["$\dot{x}_{fb, ref}$", "$\dot{y}_{fb, ref}$", "$\dot{z}_{fb, ref}$"];
-base_vel_titles = ["\textbf{Floating base $x$ velocity response}", "\textbf{Floating base $y$ velocity response}", "\textbf{Floating base $z$ velocity response}"];
-base_vel_ylabels = ["$x$-velocity [m/s]", "$y$-velocity [m/s]", "$z$-velocity [m/s]"];
+base_ang_vel_legends = ["$\dot{\phi}$", "$\dot{\theta}$", "$\dot{\psi}$"]; 
+base_ang_vel_ref_legends = ["$\dot{\phi}_{ref}$", "$\dot{\theta}_{ref}$", "$\dot{\psi}_{ref}$"];
+base_ang_vel_titles = ["\textbf{Floating base roll-rate response}", "\textbf{Floating base pitch-rate response}", "\textbf{Floating base yaw-rate response}"];
+base_ang_vel_ylabels = {["Angular Velocity"; "[deg/s]"], ["Angular Velocity"; "[deg/s]"], ["Angular Velocity"; "[deg/s]"]};
+
+base_2D_twist_legends = ["$\dot{x}_{fb}$", "$\dot{y}_{fb}$", "$\dot{\psi}$"];
+base_2D_twist_ref_legends = ["$\dot{x}_{fb, ref}$", "$\dot{y}_{fb, ref}$", "$\dot{\psi}_{ref}$"];
+base_2D_twist_titles = ["\textbf{Floating base $x$ velocity response}", "\textbf{Floating base $y$ velocity response}", "\textbf{Floating base yaw-rate response}"];
+base_2D_twist_ylabels = {"$x$-velocity [m/s]", "$y$-velocity [m/s]", ["Angular Velocity"; "[deg/s]"]};
 
 
 
@@ -115,41 +126,6 @@ end
 
 fig1 = gcf;
 
-% Floating base orientation
-figure('DefaultAxesFontSize',28)
-set(gcf, 'Position',  [100, 100, 2400, 1200])
-colororder(newcolors)
-
-for i = 1:3
-    subplot(3, 1, i)
-    hold on
-    grid on
-    
-
-    plot(state_time, 180/pi * base_pose(:,i + 3), ...
-        "LineWidth", 5, ...
-        "DisplayName", base_ori_legends(i));
-    
-    a = plot(reference_time, 180/pi * base_pose_cmd(:,i + 3), ...
-         "LineWidth", 5, ...
-         "LineStyle", '--', ...
-         "Color", 'k', ...
-         "DisplayName", base_ori_ref_legends(i));    
-   
-   
-
-    uistack(a, "top");
-    title(base_ori_titles(i), 'FontSize', 32);
-    legend('FontSize', 28);
-    xlim(xlimit);
-    ylim(base_ori_ylimits{i});
-    xlabel("Time [s]", 'FontSize', 28);
-    ylabel(base_ori_ylabels(i), 'FontSize', 28);
-    hold off
-end
-
-fig2 = gcf;
-
 % Floating base velocity
 figure('DefaultAxesFontSize',28)
 set(gcf, 'Position',  [100, 100, 2400, 1200])
@@ -183,17 +159,169 @@ for i = 1:3
     hold off
 end
 
+fig2 = gcf;
+
+% Floating base orientation
+figure('DefaultAxesFontSize',28)
+set(gcf, 'Position',  [100, 100, 2400, 1200])
+colororder(newcolors)
+
+for i = 1:3
+    subplot(3, 1, i)
+    hold on
+    grid on
+    
+
+    plot(state_time, 180/pi * base_pose(:,i + 3), ...
+        "LineWidth", 5, ...
+        "DisplayName", base_ori_legends(i));
+    
+    a = plot(reference_time, 180/pi * base_pose_cmd(:,i + 3), ...
+         "LineWidth", 5, ...
+         "LineStyle", '--', ...
+         "Color", 'k', ...
+         "DisplayName", base_ori_ref_legends(i));    
+   
+   
+
+    uistack(a, "top");
+    title(base_ori_titles(i), 'FontSize', 32);
+    legend('FontSize', 28);
+    xlim(xlimit);
+    ylim(base_ori_ylimits{i});
+    xlabel("Time [s]", 'FontSize', 28);
+    ylabel(base_ori_ylabels(i), 'FontSize', 28);
+    hold off
+end
+
 fig3 = gcf;
 
+% Floating base angular rates
+figure('DefaultAxesFontSize',28)
+set(gcf, 'Position',  [100, 100, 2400, 1200])
+colororder(newcolors)
+
+for i = 1:3
+    subplot(3, 1, i)
+    hold on
+    grid on
+    
+
+    plot(state_time(1:length(base_twist)), 180/pi * base_twist(:,i + 3), ...
+        "LineWidth", 5, ...
+        "DisplayName", base_ang_vel_legends(i));
+    
+    a = plot(reference_time, 180/pi * base_twist_cmd(:,i + 3), ...
+         "LineWidth", 5, ...
+         "LineStyle", '--', ...
+         "Color", 'k', ...
+         "DisplayName", base_ang_vel_ref_legends(i));    
+   
+   
+
+    uistack(a, "top");
+    title(base_ang_vel_titles(i), 'FontSize', 32);
+    legend('FontSize', 28);
+    xlim(xlimit);
+    ylim(base_ang_vel_ylimits{i});
+    xlabel("Time [s]", 'FontSize', 28);
+    ylabel(base_ang_vel_ylabels{i}, 'FontSize', 28);
+    hold off
+end
+
+fig4 = gcf;
+
+%% 2D twist plot
+
+% Floating base angular rates
+figure('DefaultAxesFontSize',28)
+set(gcf, 'Position',  [100, 100, 2400, 1200])
+colororder(newcolors)
+
+subplot(3, 1, 1)
+hold on
+grid on
+
+plot(state_time(1:length(base_twist)), base_twist(:,1), ...
+     "LineWidth", 5, ...
+     "DisplayName", base_2D_twist_legends(1));
+    
+a = plot(reference_time(1:length(base_twist_cmd)), base_twist_cmd(:,1), ...
+      "LineWidth", 5, ...
+      "LineStyle", '--', ...
+      "Color", 'k', ...
+      "DisplayName", base_2D_twist_ref_legends(1));  
+  
+uistack(a, "top");
+title(base_2D_twist_titles(1), 'FontSize', 32);
+legend('FontSize', 28);
+xlim(xlimit);
+ylim(base_2D_twist_ylimits{1});
+xlabel("Time [s]", 'FontSize', 28);
+ylabel(base_2D_twist_ylabels{1}, 'FontSize', 28);
+hold off
+
+subplot(3, 1, 2)
+hold on
+grid on
+
+plot(state_time(1:length(base_twist)), base_twist(:,2), ...
+     "LineWidth", 5, ...
+     "DisplayName", base_2D_twist_legends(2));
+    
+a = plot(reference_time(1:length(base_twist_cmd)), base_twist_cmd(:,2), ...
+      "LineWidth", 5, ...
+      "LineStyle", '--', ...
+      "Color", 'k', ...
+      "DisplayName", base_2D_twist_ref_legends(2));  
+  
+uistack(a, "top");
+title(base_2D_twist_titles(2), 'FontSize', 32);
+legend('FontSize', 28);
+xlim(xlimit);
+ylim(base_2D_twist_ylimits{2});
+xlabel("Time [s]", 'FontSize', 28);
+ylabel(base_2D_twist_ylabels{2}, 'FontSize', 28);
+hold off
+
+subplot(3, 1, 3)
+hold on
+grid on
+
+plot(state_time(1:length(base_twist)),180/pi * base_twist(:,6), ...
+     "LineWidth", 5, ...
+     "DisplayName", base_2D_twist_legends(3));
+    
+a = plot(reference_time(1:length(base_twist_cmd)), 180/pi * base_twist_cmd(:,6), ...
+      "LineWidth", 5, ...
+      "LineStyle", '--', ...
+      "Color", 'k', ...
+      "DisplayName", base_2D_twist_ref_legends(3));  
+  
+uistack(a, "top");
+title(base_2D_twist_titles(3), 'FontSize', 32);
+legend('FontSize', 28);
+xlim(xlimit);
+ylim(base_2D_twist_ylimits{3});
+xlabel("Time [s]", 'FontSize', 28);
+ylabel(base_2D_twist_ylabels{3}, 'FontSize', 28);
+hold off
+
+fig5 = gcf;
 
 %% Save figure
 
 filename1 = 'ho_force_z200N_pos.pdf';
-filename2 = 'ho_force_z200N_ori.pdf';
-filename3 = 'ho_force_z200N_vel.pdf';
+filename2 = 'ho_force_z200N_vel.pdf';
+filename3 = 'ho_force_z200N_ori.pdf';
+filename4 = 'ho_force_z200N_ang_vel.pdf';
+filename5 = 'ho_force_z200N_2D_twist.pdf';
 
 
 % exportgraphics(fig1, filename1)
 % exportgraphics(fig2, filename2)
 % exportgraphics(fig3, filename3)
+% exportgraphics(fig4, filename4)
+% exportgraphics(fig5, filename5)
+
 
