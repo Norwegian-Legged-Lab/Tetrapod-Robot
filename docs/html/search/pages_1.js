d5d47fdb9aab0bc82199c57480e8fa0be3882361
Paal Arthur S. Thorseth <paalthorseth@gmail.com>
2021-07-31 14:01:32 +0200

    Update docs

--- docs/html/search/pages_1.js
+++ docs/html/search/pages_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['mainpage_2518',['Mainpage',['../index.html',1,'']]],
+  ['mainpage_2518',['Mainpage',['../md_docs_markdown_mainpage.html',1,'']]],
   ['motor_20interface_2519',['Motor Interface',['../md_Arduino_libraries_motor_driver_README.html',1,'']]]
 ];
